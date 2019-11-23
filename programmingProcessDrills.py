1011afeb2c1004ec7d435c00d4cbb5854269625b
tlxiong012 <55326274+tlxiong012@users.noreply.github.com>
2019-11-23 11:39:30 -0600

    Add files via upload

---
+++ programmingProcessDrills.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 23, 2019
+
+@author: ITAUser
+'''
+
+'''
+Objective: To create a text-based story.
+''' 
+
+'''
+Algorithm:
+1. You will be given a text statement using strings, you will have to choose one of two choices using a text input.
+2. The next part of your story will be determined based on what you chose, so an if statement will be used.
+3. You can use an else statement for the other option that was chosen.
+4. Repeat steps 1-3 until the storyline is finished.
+'''
+
+'''
+Pseudo Code:
+'''
